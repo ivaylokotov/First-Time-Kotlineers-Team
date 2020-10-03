1e7147c65e548c8c694b18b73688e3fe9479f166
GGLozanov <glozanov15@gmail.com>
2020-10-03 12:08:38 +0300

    [Update] Linked Repo & Budget VM + manifest permissions

--- SpendiDly/app/src/main/java/com/example/spendidly/viewmodels/AverageBudgetFragmentViewModel.kt
+++ SpendiDly/app/src/main/java/com/example/spendidly/viewmodels/AverageBudgetFragmentViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.spendidly.viewmodels
 
 import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.test.data.BudgetX
+import kotlinx.coroutines.launch
 
 class AverageBudgetFragmentViewModel(application: Application) : BudgetFragmentViewModel(application) {
     // TODO: use database cache as single source of truth
+    fun getAverageBudgetX(): LiveData<BudgetX?> {
+        viewModelScope.launch {
+            budget = MutableLiveData(spendiDRepository.getAverageBudgetXCache())
+        }
+        return budget
+    }
 }
