e1c58648c6fdff6950e547132dea3987ff57f442
Parakatowski <a.videnov8a@gmail.com>
2020-10-03 10:59:26 +0300

    Added setup for two way databinding in the user input page.

--- SpendiDly/app/src/main/java/com/example/spendidly/viewmodels/UserInputFragmentViewModel.kt
+++ SpendiDly/app/src/main/java/com/example/spendidly/viewmodels/UserInputFragmentViewModel.kt
@@ -1,13 +1,34 @@
 package com.example.spendidly.viewmodels
 
 import android.app.Application
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
-class UserInputFragmentViewModel(application: Application) : BaseViewModel(application) {
+class UserInputFragmentViewModel(application: Application) : BaseViewModel(application), Observable {
     // TODO: Implement the ViewModel
     // two-way databinding for user input here instead of in view?
+
+    @Bindable
+    val age = MutableLiveData<String>()
+
+    @Bindable
+    val grossAnnualIncome = MutableLiveData<String>()
+
+    @Bindable
+    val members = MutableLiveData<String>()
+
+    @Bindable
+    val netAnnualIncome = MutableLiveData<String>()
+
+    @Bindable
+    val zipCode = MutableLiveData<String>()
+
+    @Bindable
+    val isHomeowner = MutableLiveData<Boolean>(false)
 
 
     // LiveData of generic Response?
@@ -15,5 +36,13 @@ class UserInputFragmentViewModel(application: Application) : BaseViewModel(appli
         viewModelScope.launch {
             // TODO: Use repository instance here and make API call;
         }
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        TODO("Not yet implemented")
     }
 }
