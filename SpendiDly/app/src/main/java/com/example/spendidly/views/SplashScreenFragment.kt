3206be8d7abb1d83c3b9330e8d47137cc7d88b4f
GGLozanov <glozanov15@gmail.com>
2020-10-03 10:50:11 +0300

    [Update] Added SplashScreen functionality & preliminary UI

--- SpendiDly/app/src/main/java/com/example/spendidly/views/SplashScreenFragment.kt
+++ SpendiDly/app/src/main/java/com/example/spendidly/views/SplashScreenFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.Handler
+import androidx.navigation.fragment.findNavController
 import com.example.spendidly.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SplashScreenFragment : Fragment() {
 
@@ -14,7 +17,25 @@ class SplashScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        // Navigate to input fragment after 3 seconds - how? TODO: Handler.postDelayed()?
+
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireActivity()
+        val bottomNav = activity.findViewById<BottomNavigationView>(R.id.bottom_nav)
+        val actionBar = activity.actionBar
+
+        bottomNav.visibility = View.GONE
+        actionBar?.hide()
+
+        // Navigate to input fragment after 3 seconds
+        view?.postDelayed({
+            bottomNav.visibility = View.VISIBLE
+            actionBar?.show()
+
+            findNavController().navigate(R.id.action_splashScreenFragment_to_userInputFragment)
+        }, 3000)
     }
 }
